3dd1591f34140707b5ebfadf073e62f0a63f7872
dragonus282 <dragonus282@gmail.com>
2023-05-29 22:58:01 +0700

    test

--- main.tf
+++ main.tf
@@ -13,30 +13,89 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-data "aws_ami" "ami" {
-  most_recent = true
+resource "aws_s3_bucket" "static" {
+  bucket        = "dragon-terraform-static-2"
+  force_destroy = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  owners = ["099720109477"]
+  tags = local.tags
 }
 
-resource "aws_instance" "server" {
-  ami           = data.aws_ami.ami.id
-  instance_type = "t3.micro"
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.static.id
 
-  lifecycle {
-    create_before_destroy = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.static.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "static" {
+  depends_on = [
+	aws_s3_bucket_public_access_block.example,
+	aws_s3_bucket_ownership_controls.example,
+  ]
+  bucket = aws_s3_bucket.static.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "static" {
+  bucket = aws_s3_bucket.static.id
+  policy = data.aws_iam_policy_document.static.json
+}
+
+resource "aws_s3_bucket_website_configuration" "static" {
+  bucket = aws_s3_bucket.static.bucket
+
+  index_document {
+    suffix = "index.html"
   }
 
+  error_document {
+    key = "error.html"
+  }
+}
+
+data "aws_iam_policy_document" "static" {
+  statement {
+    sid = "PublicReadGetObject"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.static.arn}/*"]
+
+    principals {
+      type = "*"
+      identifiers = ["*"]
+    }
+  }
+}
+
+locals {
   tags = {
-    Name = "Server"
+    hehe = "tada"
+  }
+  mime_types = {
+    html  = "text/html"
+    css   = "text/css"
+    ttf   = "font/ttf"
+    woff  = "font/woff"
+    woff2 = "font/woff2"
+    js    = "application/javascript"
+    map   = "application/javascript"
+    json  = "application/json"
+    jpg   = "image/jpeg"
+    png   = "image/png"
+    svg   = "image/svg+xml"
+    eot   = "application/vnd.ms-fontobject"
   }
 }
 
-output "public_ip" {
-  value = aws_instance.server.public_ip
+
+output "name" {
+  value= path.module
 }
